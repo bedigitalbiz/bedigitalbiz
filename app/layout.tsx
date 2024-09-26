8eb123de0e1a63dc0eb110c1355a2bad5213700b
bedigitalbiz <bedigitalbiz.info@gmail.com>
2024-09-26 09:37:13 +0700

    อัปเดต analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
-
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Be Digital Biz',
@@ -18,6 +18,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
